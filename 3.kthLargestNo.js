d14db1bd302b11ac61d6f0e6b4216b6d9358ad1b
kapilkatkar <kapilkatkar32@gamil.com>
2023-08-19 14:31:18 +0530

    tried edge cases for kth largestNo

--- 3.kthLargestNo.js
+++ 3.kthLargestNo.js
@@ -35,4 +35,4 @@ const findKthLargest = (nums, k) => {
   console.log(nums);
   return nums[k - 1];
 };
-console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6, -2, -7, -6], 10));
